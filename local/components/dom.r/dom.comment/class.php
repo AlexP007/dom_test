b2e6c4689530ad3d3446a9b817499f33fa55ad8b
AlexP <alex.p.panteleev@gmail.com>
2020-06-06 19:23:05 +0300

    [u] class.php add getComments()

--- local/components/dom.r/dom.comment/class.php
+++ local/components/dom.r/dom.comment/class.php
@@ -7,6 +7,37 @@ class DomComment extends CBitrixComponent
 {
     public function executeComponent()
     {
+        // fetching comments
+        $this->arResult['COMMENTS'] = $this->getComments();
+
         $this->includeComponentTemplate();
+    }
+
+    private function getComments()
+    {
+        $result = [];
+
+        $dbRes = CIBlockElement::GetList(
+            [
+                "SORT"=>"ASC"
+            ],
+            [
+                "IBLOCK_ID" => (int) $this->arParams['IBLOCK_ID'],
+                "PROPERTY_{$this->arParams['RELATED_FIELD']}" => "{$this->arParams['ELEMENT_ID']}",
+                "ACTIVE" => "Y",
+            ],
+            false,
+            false,
+            [
+                'DETAIL_TEXT',
+                'DATE_CREATE',
+            ]
+        );
+
+        while ($elt = $dbRes->GetNext() ) {
+            $result[] = $elt;
+        }
+
+        return $result;
     }
 }
