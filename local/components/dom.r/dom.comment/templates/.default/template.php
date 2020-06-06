4a4f827553480430fe4f32f12a31d54e5522a929
AlexP <alex.p.panteleev@gmail.com>
2020-06-06 21:37:35 +0300

    [u] template and add lang for it
    Took 11 minutes

--- local/components/dom.r/dom.comment/templates/.default/template.php
+++ local/components/dom.r/dom.comment/templates/.default/template.php
@@ -1,12 +1,27 @@
 <?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-    print_r($arParams);
-    print_r($arResult);
+
+use Bitrix\Main\Localization\Loc;
+
 ?>
 
+<h3><?= Loc::getMessage('DOM_COMMENT_TEMPLATE_HEADING_MAIN') ?></h3>
+<ul>
+    <?php if (count($arResult['COMMENTS']) > 0): ?>
+        <?php foreach ($arResult['COMMENTS'] as $comment): ?>
+        <li>
+            <?= $comment['~DATE_CREATE'] ?>
+            <br>
+            <?= $comment['~DETAIL_TEXT'] ?></li>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</ul>
 
+<h3><?= Loc::getMessage("DOM_COMMENT_TEMPLATE_HEADING_SUB") ?></h3>
 <form method="POST">
     <textarea name="new_comment" id="" cols="30" rows="10"></textarea>
-    <button type="submit">Добавить коммент</button>
+    <button type="submit">
+        <?= Loc::getMessage("DOM_COMMENT_TEMPLATE_BUTTON") ?>
+    </button>
 </form>
 
 
