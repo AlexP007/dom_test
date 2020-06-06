432dfe961ae5765a784de503cf1b48749409ec11
AlexP <alex.p.panteleev@gmail.com>
2020-06-06 22:30:28 +0300

    [i]
    Took 10 minutes

---
+++ local/components/dom.r/dom.detail/lang/ru/templates/.default/template.php
@@ -0,0 +1,3 @@
+<?php
+$MESS['DOM_DETAIL_TEMPLATE_HEADING_SUB'] = "Элемент";
+?>
--- local/components/dom.r/dom.detail/templates/.default/template.php
+++ local/components/dom.r/dom.detail/templates/.default/template.php
@@ -1,8 +1,23 @@
-<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); ?>
+<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
-<h1>Детальная страница элемента</h1>
-<h2>Элемент <?= $arResult['ELEMENT']['NAME'] ?></h2>
+use Bitrix\Main\Localization\Loc;
+
+?>
+
+<h2><?= Loc::getMessage('DOM_DETAIL_TEMPLATE_HEADING_SUB') ?> <?= $arResult['ELEMENT']['NAME'] ?></h2>
 <img src=<?= $arResult['ELEMENT']['DETAIL_PICTURE']['src'] ?>>
 <p><?= $arResult['ELEMENT']['PREVIEW_TEXT'] ?></p>
 <p><?= $arResult['ELEMENT']['DETAIL_TEXT'] ?></p>
+
+<?$APPLICATION->IncludeComponent(
+    "dom.r:dom.comment",
+    "",
+    Array(
+        "CACHE_TIME" => "36000000",
+        "CACHE_TYPE" => "A",
+        "ELEMENT_ID" => $arParams['ELEMENT_ID'],
+        "IBLOCK_ID" => "6",
+        "RELATED_FIELD" => "RELATED_COMMENT_ID"
+    )
+);?>
 
